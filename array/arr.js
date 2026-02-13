2e94d3fa5a3e877b69f2ea287b15fe434d3cb8e9
sudhanshu015 <sudhanshukumar1139@gmail.com>
2026-02-13 23:24:54 +0530

    array

---
+++ array/arr.js
@@ -0,0 +1,85 @@
+// let marks =[98, 86, 45, 67 ,45 ];
+// marks[3]=43;
+// console.log( marks);
+// console.log( " lenght ", marks.length);
+
+
+//  let heroes = [ " iron man", " spider man ", " hulk "," balck panther "];
+//  for (let i =0; i<heroes.length; i++)
+//  {
+//     console.log(" name of heroes is ", heroes[i]);
+//  }
+
+//        //// for of loop ///
+
+//        for( let hero of heroes)
+//        {
+//         console.log(hero);
+//        }
+
+
+// let cities =[ " delhi", "mumbie", " patana", " gaye"];
+// for (let city of cities){
+//     console.log(city.toUpperCase());
+// }
+
+
+       ///// QUESTION ///
+
+
+//  let marks = [ 85 , 97,44, 37,76,60];
+ 
+//  let sum = 0;
+//  for( let i =0 ; i<marks.length;i++ )
+//  {
+//     sum+= marks[i];
+//  }
+
+//  console.log(sum)
+
+//   let avg = sum/marks.length;
+//    console.log(` avg marks of  the class = ${avg}`);
+
+       ///// QUESTION //
+       
+       
+
+      //  let items = [ 250,645,300,900,50];//
+
+      // let i = 0;
+      //  for ( let val of items)
+      //  {
+      //    // console.log(`value at indet ${i }= ${val}`);
+      //    let offer = val/10
+      //    items[i]= items[i]- offer;
+      //    console.log(` value after offer = ${items[i]}`)
+      //    i++ ;
+      //  }
+
+      //   let items = [ 250,645,300,900,50];
+      //  for( let i = 0 ; i< items.length;i++)
+      //  {
+      //     let offer= items[i]/10;
+      //    items[i] -= offer ;
+      //  }
+      //  console.log(items);
+
+//  let foodItem = [ "mango", "lichi", "banana", "patato", " lily"]
+//  console.log(foodItem.push("apple"))   
+
+// let arr = [1,2,3,4,5,6,7]
+// //  arr.splice()
+// // add element
+//  arr.splice(2 ,0,34)
+
+//  // delete 
+//  arr.splice(3,1)
+
+//  // replace
+//   arr.splice(3,1,645)
+
+let company = [ "bloomberg", "microsoft", " uber", " Google", " IBM", "Netflix"];
+
+company.shift();
+company.splice(2,1,"ola");
+company.push(" amozan");
