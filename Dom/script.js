b7b4e480d98ed7f5ee3e3ba9f984d0e58ff8b6b8
sudhanshu015 <sudhanshukumar1139@gmail.com>
2026-02-16 02:40:50 +0530

    dom1

---
+++ Dom/script.js
@@ -0,0 +1,46 @@
+// console.log("hellow")
+// // alert("hellow")
+// console.dir(window.Document);
+//  let red=document.getElementById("jaiho"); // h1
+//   console.log(red);
+// let heading= document.getElementsByClassName("jaiho")  
+// console.dir(heading)
+// console.log(heading)
+
+// let para =document.getElementsByTagName("p");
+// console.dir(para);
+
+//  let firelement = document.querySelector("p")// with the use of quearySelector we axis all  1.tags 2. byid 3.myclass
+//  console.dir(firelement);
+//   let allelemnt = document.querySelectorAll("p")
+//   console.dir(allelemnt);
+
+// console.div(document.body.firstChild);
+   
+// let  mydiv = document.querySelector("div");
+// console.dir(mydiv);
+
+// let heading=document.querySelector("h1");
+// console.dir(heading);
+// let h2=document.querySelector("h2")
+
+// h2.innerText=h2.innerText  +  "from vgi"
+
+// console.dir(h2.innerText);
+
+let divs = document.querySelectorAll(".box");
+divs[0].innerHTML="newgr"
+divs[1].innerHTML="ouuyr"
+divs[2].innerHTML="effg"
+
+
+
+
+
+
+
+
+
+
+
+
