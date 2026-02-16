25feb936008a36dac6482f33ce3a672d5c84fa94
sudhanshu015 <sudhanshukumar1139@gmail.com>
2026-02-17 00:04:24 +0530

    dom2

---
+++ DOM2/dom.js
@@ -0,0 +1,51 @@
+//  let div = document.querySelector("div");
+//  console.dir(div);
+
+// let id =div.getAttribute("id");
+// console.dir(id);
+
+// let name =div.getAttribute("name");
+// console.log(name);
+
+// let lara=document.querySelector("p")
+// console.log(lara.getAttribute("class"));
+
+// let para=document.querySelector("p")
+// console.log(para.setAttribute("class","newclass"));
+
+   
+// FOR END IN NOOD //
+// let newbtn=document.createElement("button");
+// newbtn.innerText="click me"
+//  console.log(newbtn);
+//  let div = document.querySelector("div")
+ // div.append(newbtn)// for end 
+ // div.prepend(newbtn)  FOR START 
+// div.after(newbtn);  FOR AFTER
+// div.before(newbtn); for before
+
+// let newheading = document.createElement("h1");
+// newheading.innerHTML= "<i>i am new !</i>";
+// document.querySelector("body").prepend(newheading);
+
+// let para=document.querySelector("p");
+// para.remove();
+
+
+
+      //// QUEASTUION ////
+
+    //   let butt=document.createElement("button");
+    //   butt.innerText="click me"
+    //   butt.style.color="white";
+    //   butt.style.background="pink";
+    //   console.log(butt);
+
+    //   let bod=document.querySelector("body");
+    //   bod.prepend(butt);
+
+
+      //// QUEATION 2 ////
+
+      let pare = document.querySelector("p");
+      
